52670863cd9e3b7a790d8d19cb65b2888e61e602
SrcRPG <114306981+SrcRPG@users.noreply.github.com>
2022-10-31 15:41:24 +0100

    Create main.c

---
+++ TasksMutexSemaphore/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+
+SemaphoreHandle_t xSemaphore = NULL;
+
+void vTask1()
+{
+  xSemaphoreTake(xSempahore, portMAX_DELAY);
+  for (size_t i = 0; i < 3; i++)
+  {
+    printf("Task3 %d: %lld\n", i, esp_timer_get_time() / 1000);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+  printf("\n");
+  xSemaphoreGive(xSemaphore);
+  vTaskDelete(NULL);
+}
+
+void vTask2()
+{
+  xSemaphoreTake(xSempahore, portMAX_DELAY);
+  for (size_t i = 0; i < 3; i++)
+  {
+    printf("Task3 %d: %lld\n", i, esp_timer_get_time() / 1000);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+  printf("\n");
+  xSemaphoreGive(xSemaphore);
+  vTaskDelete(NULL);
+}
+
+void vTask3()
+{
+  xSemaphoreTake(xSempahore, portMAX_DELAY);
+  for (size_t i = 0; i < 3; i++)
+  {
+    printf("Task3 %d: %lld\n", i, esp_timer_get_time() / 1000);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+  printf("\n");
+  xSemaphoreGive(xSemaphore);
+  vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+  printf("\n");
+  xSemaphore = xSemaphoreCreateMutex();
+  
+  xTaskCreate(vTask1, "Task1", 2048, NULL, 2, NULL);
+  xTaskCreate(vTask2, "Task2", 2048, NULL, 2, NULL);
+  xTaskCreate(vTask3, "Task3", 2048, NULL, 2, NULL);
+}
